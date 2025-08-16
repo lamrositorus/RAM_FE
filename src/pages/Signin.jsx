cc15f29601d1f537410447cd08994b6784b40d66
lamrositorus <lamrositorus13@gmail.com>
2025-08-16 10:14:20 +0700

    updated json untuk halaman refresh

--- src/pages/Signin.jsx
+++ src/pages/Signin.jsx
@@ -26,7 +26,7 @@ export const Signin = () => {
       const token = await signin({ email, password })
       login(token)
       toast.success('Login berhasil')
-      navigate('/api/dashboard')
+      navigate('/dashboard')
     } catch (err) {
       const msg = err.message
       setError(msg)
